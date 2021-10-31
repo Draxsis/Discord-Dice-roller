9faf5274833d3993282fb417e2cef1b72b0970c6
Mostafa Koolabadi <93474063+Draxsis@users.noreply.github.com>
2021-10-31 18:28:56 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,65 @@
+import random
+import discord
+import asyncio
+from discord.ext import commands
+
+client = commands.Bot (command_prefix = "$" , activity = discord.Game(name="Persian D&D | $help ")) 
+
+
+@client.command()
+async def roll_1d4(ctx):
+    
+    n = (f"**{random.randint(1, 4)}**")
+    
+    embed = discord.Embed(title= (':game_die: your result for 1d4 is : ')+(n)) 
+    await ctx.send(embed=embed)
+
+@client.command()
+async def roll_1d6(ctx):
+    
+    n = (f"**{random.randint(1, 6)}**")
+    
+    embed = discord.Embed(title= (':game_die: your result for 1d6 is : ')+(n)) 
+    await ctx.send(embed=embed)
+
+@client.command()
+async def roll_1d8(ctx):
+    
+    n = (f"**{random.randint(1, 8)}**")
+    
+    embed = discord.Embed(title= (':game_die: your result for 1d8 is : ')+(n)) 
+    await ctx.send(embed=embed)
+
+@client.command()
+async def roll_1d10(ctx):
+    
+    n = (f"**{random.randint(1, 10)}**")
+    
+    embed = discord.Embed(title= (':game_die: your result for 1d10 is : ')+(n)) 
+    await ctx.send(embed=embed)
+
+@client.command()
+async def roll_1d12(ctx):
+    
+    n = (f"**{random.randint(1, 12)}**")
+    
+    embed = discord.Embed(title= (':game_die: your fresult for 1d12 is : ')+(n)) 
+    await ctx.send(embed=embed)
+
+@client.command()
+async def roll_1d20(ctx):
+    
+    n = (f"**{random.randint(1, 20)}**")
+    
+    embed = discord.Embed(title= (':game_die: your result for 1d20 is : ')+(n)) 
+    await ctx.send(embed=embed)
+
+@client.command()
+async def roll_1d100(ctx):
+    
+    n = (f"**{random.randint(1, 100)}**")
+    
+    embed = discord.Embed(title= (':game_die: your result for 1d100 is : ')+(n)) 
+    await ctx.send(embed=embed)
+
+client.run('ODgxMDY4NzE5NzMwNzUzNTM2.YSndMA.sZwQ0KZAycxG1HupfS0vE0lD5PQ')
