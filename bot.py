9a3390add4abdc598b4e65b6890d09a68edfcd85
Mostafa Koolabadi <93474063+Draxsis@users.noreply.github.com>
2022-01-17 17:11:08 +0330

    main bot.py file
    added a new feature called "class"

--- bot.py
+++ bot.py
@@ -4,32 +4,29 @@
 # version : 1.0.0
 # author : Draxsis / Mostafa Koolabadi
 
+from ast import alias
 import os
-import discord 
+import platform
+from pydoc import describe
+import discord
 import d20
-from d20 import roll
-from d20 import SimpleStringifier
+from d20 import *
 from discord import embeds 
 from discord.ext import commands
-
-#------------------------------- START ---------------------------------------
+import requests
+from bs4 import BeautifulSoup
 
 client = commands.Bot (command_prefix = "$" , description="This bot will help you to roll all types of dices! ", activity = discord.Game(name="Persian D&D | $help "))
 
 @client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------\n Bot is Online and ready for anything ^^ \n . \n . \n .')
-
-def is_me(m):
-    return m.author == client.user
-    
-#------------------------------ ROLLS DICE ------------------------------------
+async def on_ready(): #showing the bot status
+    print(f"Logged in as {client.user.name}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
 
 @client.command(name='roll', aliases=['r'])
-async def Roll(ctx, *, dice: str = '1d20'):
+async def Roll(ctx, *, dice: str = '1d20'): #main dice roller
     
     dice = (dice)
     res = d20.roll(dice, stringifier= SimpleStringifier())
@@ -43,9 +40,8 @@ async def Roll(ctx, *, dice: str = '1d20'):
     embed = discord.Embed(title= (':game_die: your die result is:\n'), description= (f'Total : ** {out} **'))
     await ctx.send(myid, embed=embed)
 
-#------------------------------ RANDCHAR ------------------------------------
 @client.command(name='randchar', aliases=['rch'])
-async def randchar(ctx):
+async def randchar(ctx): # randomize 6 stats for character (4d6kh3 x 6)
 
     rolls = [roll("4d6kh3") for _ in range(6)]
     stats = '\n'.join(str(r) for r in rolls)
@@ -54,11 +50,36 @@ async def randchar(ctx):
     author_id = ctx.message.author.id
     myid = f'<@{author_id}>'
 
-    await ctx.message.delete()
-
     embed = discord.Embed(title= (':game_die: Generated random stats:\n'), description= (f'\n{stats}\n\n **Total = {total}**'))
+    await ctx.message.delete()             
     await ctx.send(myid, embed=embed)
 
+@client.command(name='class')
+async def Klass(ctx, class_name): #returning results from wikidot website and show details for each class
+    
+    URL = f"http://dnd5e.wikidot.com/{class_name}"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.text, 'lxml')
+    
+    info = soup.select("p")[0].getText()
+    hp = soup.select("p")[3].getText()
+    prof = soup.select("p")[4].getText()
+    equip1 = soup.select("li")[26].getText()
+    equip2 = soup.select("li")[27].getText()
+    equip3 = soup.select("li")[28].getText()
+    equip4 = soup.select("li")[29].getText()
+ 
+    embed=discord.Embed(title=f"{class_name} - D&D 5e edition",
+                    url=f"http://dnd5e.wikidot.com/{class_name}",
+                    description= info)
+    embed.add_field(name="Hit Points", value=hp, inline=False)
+    embed.add_field(name="Proficiencies", value=prof, inline=False)
+    embed.add_field(name="Equipments", value=(f'{equip1} \n {equip2} \n {equip3} \n {equip4}'), inline=False)
+    embed.set_footer(text="powered by Draxsis from dnd5e.wikidot")
+
+    await ctx.message.delete()
+    await ctx.send(embed=embed)
+    
     
 TOKEN = os.getenv("DISCORD_TOKEN")
 client.run(TOKEN)
